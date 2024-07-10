600e3ccd328b26fdf478b6b9e01fb1b5acdd3ccf
cobitameli@gmail.com <melicoba94@hotmail.com>
2024-07-09 19:53:04 -0500

    ejercicio2

---
+++ ejercicio11.py
@@ -0,0 +1,4 @@
+#Invertir una cadena: Escribe un programa que invierta una cadena introducida por el usuario.
+cadena=input("ingrese texto: ")
+invierta = cadena[::-1]
+print(invierta)
---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+#Suma de dos números: Escribe un programa que pida al usuario dos números y los sume.
+
+num1=int(input("Ingrese un numero: "))
+num2=int(input("Ingrese otro numero: "))
+print(num1+num2)
